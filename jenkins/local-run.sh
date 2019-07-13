43b87091b35758cfd189ca65034fa3d98becda96
吴晓鹏 <wuxiaopeng@meizhitec.com>
2019-07-13 17:12:18 +0800

    fix bug

--- jenkins/local-run.sh
+++ jenkins/local-run.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
-# 第一个参数为 环境 env 第二个参数 需要执行的features 列表
+# 1.第一个参数为 环境 env 第二个参数 需要执行的features 列表
 if [[ ! -n $1 ]]&&[[ ! -n $2 ]]; then
     echo "please input env and features"
     exit 1
 fi
 
-
-# 本地运行不需要这个
+## 2.当前目录切换python 环境，如果在本地调试，不需要这一步
 #. /etc/profile.d/pyenv.sh
 #python --version
 #pyenv local 3.7.2
 #python --version
-# 移除旧的报告文件夹
-if [[ $1 == 'test' ]]; then
+# 3.导入环境变量
+if [ "$1" = "test" ]; then
     export TestEnv='test'
-elif [[ $1 == 'uat' ]]; then
+elif [ "$1" = "uat" ]; then
     export TestEnv='uat'
 else
     echo "please input test or uat"
     exit 1
 fi
-
+# 4.移除旧的报告文件夹
 if [ -d allure_results ]; then
    rm -rf allure_results
 fi
 
-for i in $2 ;do
-    behave -f allure_behave.formatter:AllureFormatter -o allure_results $i --tags=$1
+# 5.运行测试
+for i in $2
+do
+  behave -f allure_behave.formatter:AllureFormatter -o allure_results $i --tags=$1
 done
--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 # 1.第一个参数为 环境 env 第二个参数 需要执行的features 列表
-if [ ! -n $1 ]&&[ ! -n $2 ]; then
+if [[ ! -n $1 ]]&&[[ ! -n $2 ]]; then
     echo "please input env and features"
     exit 1
 fi
 
-# 2.当前目录切换python 环境，如果在本地调试，不需要 这一步
+# 2.当前目录切换python 环境，如果在本地调试，不需要这一步
 . /etc/profile.d/pyenv.sh
 python --version
 pyenv local 3.7.2
 python --version
+# 3.导入环境变量
 if [ "$1" = "test" ]; then
     export TestEnv='test'
 elif [ "$1" = "uat" ]; then
@@ -18,12 +19,13 @@ else
     echo "please input test or uat"
     exit 1
 fi
-# 3.移除旧的报告文件夹
+# 4.移除旧的报告文件夹
 if [ -d allure_results ]; then
    rm -rf allure_results
 fi
 
-# 4.运行测试
-for i in $2;do
+# 5.运行测试
+for i in $2
+do
   behave -f allure_behave.formatter:AllureFormatter -o allure_results $i --tags=$1
 done
