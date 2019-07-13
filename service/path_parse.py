05fa18fcffa562e89fd0cf039ef8297d7afced3c
吴晓鹏 <wuxiaopeng@meizhitec.com>
2019-07-13 15:54:19 +0800

    add run shell

--- service/path_parse.py
+++ service/path_parse.py
@@ -1,2 +1,8 @@
 #encoding=utf-8
+import  re
 
+def path_join(path,tenant_code):
+    path_list = re.split('{|}', path)
+    path_list[1] = tenant_code
+    path = ''.join(path_list)
+    return path
