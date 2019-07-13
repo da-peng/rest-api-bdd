f0f5576cf36ee3e7e0577e701cde1998100fa7c4
吴晓鹏 <wuxiaopeng@meizhitec.com>
2019-07-13 16:13:37 +0800

    add description

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-# 第一个参数为 环境 env 第二个参数 需要执行的features 列表
+# 1.第一个参数为 环境 env 第二个参数 需要执行的features 列表
 if [[ ! -n $1 ]]&&[[ ! -n $2 ]]; then
     echo "please input env and features"
     exit 1
 fi
 
+# 2.当前目录切换python 环境，如果在本地调试，可以 这一步
 . /etc/profile.d/pyenv.sh
 python --version
 pyenv local 3.7.2
 python --version
-# 移除旧的报告文件夹
 
+# 3.移除旧的报告文件夹
 if [ -d allure_results ]; then
    rm -rf allure_results
 fi
 
+# 4.运行测试
 for i in $2
 do
   behave -f allure_behave.formatter:AllureFormatter -o allure_results $i --tags=$1
