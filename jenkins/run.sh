ec322b55cda978d51c25da255517b448f49c76ad
吴晓鹏 <wuxiaopeng@meizhitec.com>
2019-07-15 14:51:18 +0800

    fix bug

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -32,7 +32,7 @@ for i in $@
 do
   echo $i
   # 变量名和等号之间不能有空格
-  if [ $j = 2 ]||[ $j > 2 ]; then
+  if [ $j -eq 2 ]||[ $j -gt 2 ]; then
     behave -f allure_behave.formatter:AllureFormatter -o allure_results $i --tags=$1
   fi
   j=$(($j+1))
