0630a44f33edeeeb25eb4b0596b525d89614df03
吴晓鹏 <wuxiaopeng@meizhitec.com>
2019-07-15 15:39:56 +0800

    加入时区设置

--- utils/time_mange.py
+++ utils/time_mange.py
@@ -1,22 +1,36 @@
 #encoding=utf-8
 import time
-import datetime
+from datetime import datetime, timedelta, timezone
+
+
+def getCurrentDateTime():
+    # 拿到UTC时间，并强制设置时区为UTC+0:00:
+    utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
+    # astimezone()将转换时区为北京时间:
+    bj_dt = utc_dt.astimezone(timezone(timedelta(hours=8)))
+    return bj_dt
 
 def getCurrentTime():
-    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
+    return getCurrentDateTime().strftime("%Y-%m-%d %H:%M:%S")
 
 def sleep():
     time.sleep(1)
 
 def getEndTime():
-    return (datetime.datetime.now() + datetime.timedelta(hours=1)).strftime("%Y-%m-%d %H:%M:%S")
+    return (getCurrentDateTime() + timedelta(hours=1)).strftime("%Y-%m-%d %H:%M:%S")
 
 
 def get_time_stamp():
     ct = time.time()
-    local_time = time.localtime(ct)
+    local_time = getCurrentTime()
     # data_head = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
     data_head = time.strftime("%M-%S", local_time)
     data_secs = str(ct)[-3:]
     time_stamp = "%s.%s" % (data_head, data_secs)
     return time_stamp
+
+
+if __name__ =='__main__':
+    print(getCurrentTime())
+    print(getEndTime())
